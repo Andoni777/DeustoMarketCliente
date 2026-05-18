08521ef4cd1699915cc96e83e5539722467961f2
Andoni Gonzalez <andoni.g@opendeusto.es>
2026-05-18 22:58:54 +0200

    cambios menores

--- DeustoMarketCliente/src/Interfaz.cpp
+++ DeustoMarketCliente/src/Interfaz.cpp
@@ -1,8 +1,8 @@
 /*
  * Interfaz.cpp
  *
- *  Created on: 11 may 2026
- *      Author: andoni.g
+ * Created on: 11 may 2026
+ * Author: andoni.g
  */
 
 #include "Interfaz.h"
@@ -24,47 +24,47 @@ int Interfaz::leerOpcion(){
 
 int Interfaz::mostrarMenuGestSuper(){
 	std::cout << "\n =================================================";
-	std::cout << "\n       DEUSTOMARKET - GESTIÓN DE SUPERMERCADOS     ";
+	std::cout << "\n       DEUSTOMARKET - GESTION DE SUPERMERCADOS     ";
 	std::cout << "\n =================================================";
-	std::cout << "\n 1. Añadir nuevo supermercado";
+	std::cout << "\n 1. Anadir nuevo supermercado";
 	std::cout << "\n 2. Modificar supermercado existente";
 	std::cout << "\n 3. Eliminar supermercado";
 	std::cout << "\n 4. Mostrar lista de supermercados";
-	std::cout << "\n 0. Salir al menú principal";
+	std::cout << "\n 0. Salir al menu principal";
 	std::cout << "\n =================================================";
-	std::cout << "\n\nIntroduce el número de tu selección: ";
+	std::cout << "\n\nIntroduce el numero de tu seleccion: ";
 
 	return leerOpcion();
 }
 
 int Interfaz::mostrarMenuGestIyP(){
 	std::cout << "\n =====================================================";
-	std::cout << "\n       DEUSTOMARKET - GESTIÓN DE INVENTARIO/PRODUCTOS ";
+	std::cout << "\n       DEUSTOMARKET - GESTION DE INVENTARIO/PRODUCTOS ";
 	std::cout << "\n =====================================================";
-	std::cout << "\n 1. Añadir nuevo producto al catálogo";
+	std::cout << "\n 1. Anadir nuevo producto al catalogo";
 	std::cout << "\n 2. Eliminar producto";
 	std::cout << "\n 3. Modificar precio de producto";
 	std::cout << "\n 4. Actualizar stock de un supermercado";
 	std::cout << "\n 5. Mostrar stock de supermercados";
-	std::cout << "\n 0. Salir al menú principal";
+	std::cout << "\n 0. Salir al menu principal";
 	std::cout << "\n =====================================================";
-	std::cout << "\n\nIntroduce el número de tu selección: ";
+	std::cout << "\n\nIntroduce el numero de tu seleccion: ";
 
 	return leerOpcion();
 	}
 
 int Interfaz::mostrarMenuGestEmpleado(){
 	std::cout << "\n =========================================";
-	std::cout << "\n       DEUSTOMARKET - GESTIÓN DE PERSONAL  ";
+	std::cout << "\n       DEUSTOMARKET - GESTION DE PERSONAL  ";
 	std::cout << "\n =========================================";
 	std::cout << "\n 1. Dar de alta nuevo empleado";
 	std::cout << "\n 2. Dar de baja (eliminar) empleado";
     std::cout << "\n 3. Modificar datos de empleado";
     std::cout << "\n 4. Mostrar lista de todos los empleados";
-    std::cout << "\n 5. Mostrar lista de empleados de un supermercado específico";
-    std::cout << "\n 0. Salir al menú principal";
+    std::cout << "\n 5. Mostrar lista de empleados de un supermercado especifico";
+    std::cout << "\n 0. Salir al menu principal";
     std::cout << "\n =========================================";
-    std::cout << "\n\nIntroduce el número de tu selección: ";
+    std::cout << "\n\nIntroduce el numero de tu seleccion: ";
 
 	return leerOpcion();
 }
@@ -73,12 +73,12 @@ int Interfaz::mostrarMenu(){
 	std::cout << "\n ==========================================";
 	std::cout << "\n       DEUSTOMARKET - PANEL DE CONTROL     ";
 	std::cout << "\n ==========================================";
-	std::cout << "\n1. Gestión de Supermercados";
-	std::cout << "\n2. Gestión de Inventario y Productos";
-	std::cout << "\n3. Gestión de Empleados";
+	std::cout << "\n1. Gestion de Supermercados";
+	std::cout << "\n2. Gestion de Inventario y Productos";
+	std::cout << "\n3. Gestion de Empleados";
 	std::cout << "\n4. Ver Logs del Sistema";
 	std::cout << "\n0. Salir";
-	std::cout << "\n\nIntroduce el numero de tu selección: ";
+	std::cout << "\n\nIntroduce el numero de tu seleccion: ";
 
 	return leerOpcion();
 }
